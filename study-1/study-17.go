31f438087c65a33d91012e1f7df6bb1d5c55aee7
kingkun <kingkun1234@163.com>
2017-09-23 22:43:29 +0800

    study

--- study-1/study-17.go
+++ study-1/study-17.go
@@ -7,7 +7,19 @@ import (
 )
 
 func main() {
-	urlfunc()
+	//urlfunc()
+	countHeight()
+}
+
+//一球从100米高度自由落下，每次落地后反跳回原高度的一半；再落下，求它在第10次落地时，共经过多少米？第10次反弹多高？
+func countHeight() {
+	h, s := 100, 100
+	for i := 1; i <= 10; i++ {
+		s += h
+		h /= 2
+	}
+	fmt.Println("经过的路程：", s)
+	fmt.Println("反弹的高度:", h)
 }
 
 type Node struct {
@@ -99,22 +111,22 @@ func (l *List) NomalIndex(index int) int {
 	return index
 }
 
-func (l *List) Range(start, end int) []interface{} {
-	start = l.NomalIndex(start)
-	end = l.NomalIndex(end)
-	res := []interface{}{}
-	if start > end {
-		return res
-	}
-	sNode := l.index(start)
-	eNode := l.index(end)
-	for n := sNode; n != eNode; {
-		res = append(res, n.Value)
-		n = n.next
-	}
-	res = append(res, eNode.Value)
-	return res
-}
+// func (l *List) Range(start, end int) []interface{} {
+// 	start = l.NomalIndex(start)
+// 	end = l.NomalIndex(end)
+// 	res := []interface{}{}
+// 	if start > end {
+// 		return res
+// 	}
+// 	sNode := l.index(start)
+// 	eNode := l.index(end)
+// 	for n := sNode; n != eNode; {
+// 		res = append(res, n.Value)
+// 		n = n.next
+// 	}
+// 	res = append(res, eNode.Value)
+// 	return res
+// }
 
 func urlfunc() {
 	urlstr := "postgres://user:pass@host.com:5432/path?k=v#f"
