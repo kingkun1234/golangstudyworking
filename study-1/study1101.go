8cd7936abfa9cba7dbe1bd29a5a32eef153638d1
kingkun <kingkun1234@163.com>
2017-11-01 22:35:50 +0800

    study

--- study-1/study1101.go
+++ study-1/study1101.go
@@ -1,5 +1,23 @@
 package main
 
-func main() {
+import (
+	"encoding/json"
+	"fmt"
+)
 
+func main() {
+	user := Customer{"king", "kingkun", "1234567890", 123, 23}
+	str, _ := json.Marshal(user)
+	fmt.Println(string(str))
+	var customer Customer
+	json.Unmarshal(str, &customer)
+	fmt.Println(customer)
+}
+
+type Customer struct {
+	UserName     string `json:"userName"`
+	UserAccount  string `json:"userAccount"`
+	UserMobile   string `json:"userMobile"`
+	VotesNumber  int    `json:"votesNumber"`
+	ActivityRank int    `json:"activityRank"`
 }
